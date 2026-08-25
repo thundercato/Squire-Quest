fdc633d5acc7f2e105f0ac30d538cfbc34824dc8
Adam <thundercato@users.noreply.github.com>
2026-08-25 22:17:46 +0100

    Style room descriptions as illuminated manuscript text

--- game/settings.js
+++ game/settings.js
@@ -2,13 +2,13 @@
 
 settings.title = 'Squire Quest I: The Dragon Slayer'
 settings.author = 'Adam and Colin of GAC Software Inc.'
-settings.version = '0.1.1'
+settings.version = '0.1.2'
 settings.panes = 'none'
 settings.compassPane = false
 settings.statusPane = false
 settings.inventoryPane = []
 settings.roomTemplate = [
-  '{terse:{hereDesc}}',
+  '{class:room-description:{terse:{hereDesc}}}',
   '{objectsHere:You can see {objects} here.}',
   '{exitsHere:You can go {exits}.}',
 ]
