2398bb90781babd58555b0d274ffc7cb7d92040f
Adam <thundercato@users.noreply.github.com>
2026-08-26 08:52:10 +0100

    Load new audio engine and bump build version

--- game/settings.js
+++ game/settings.js
@@ -2,12 +2,12 @@
 
 settings.title = 'Squire Quest I: The Dragon Slayer'
 settings.author = 'Adam and Colin of GAC Software Inc.'
-settings.version = '0.2.3'
+settings.version = '0.2.4'
 settings.panes = 'none'
 settings.compassPane = false
 settings.statusPane = false
 settings.inventoryPane = []
-settings.files = ['code', 'data', 'artwork-map', 'puzzle-core', 'puzzle-chicken', 'puzzle-forest', 'puzzle-final', 'puzzle-dungeon', 'puzzle-polish', 'market-hotfix']
+settings.files = ['audio-engine', 'code', 'data', 'artwork-map', 'puzzle-core', 'puzzle-chicken', 'puzzle-forest', 'puzzle-final', 'puzzle-dungeon', 'puzzle-polish', 'market-hotfix']
 settings.roomTemplate = [
   '{class:room-description:{terse:{hereDesc}}}',
   '{objectsHere:You can see {objects} here.}',
